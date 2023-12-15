911966244d8ef86782c1ef02d3e4cdc3e74a3a25
cnlund <91978441+cnlund@users.noreply.github.com>
2023-12-15 03:13:01 -0500

    avance 3

--- private/randlol.go
+++ private/randlol.go
@@ -6,8 +6,21 @@ func inicioHandler(c *fiber.Ctx) error {
 	return c.SendFile("public/inicio.html")
 }
 
+func randlolHandler(c *fiber.Ctx) error {
+	return c.SendFile("public/randlol.html")
+}
+
 func main() {
 	web := fiber.New()
 	web.Get("/", inicioHandler)
+	web.Static("/css", "/public/inicio.css")
+	web.Post("/randlol", randlolHandler)
+	web.Static("/randlol/css", "/public/randlol.css")
+	web.Static("/randlol/todas", "/public/imagenes-randlol/emblem-challenger.png")
+	web.Static("/randlol/top", "/public/imagenes-randlol/Position_Gold-Top.png")
+	web.Static("/randlol/jg", "/public/imagenes-randlol/Position_Gold-Jungle.png")
+	web.Static("/randlol/mid", "/public/imagenes-randlol/Position_Gold-Mid.png")
+	web.Static("/randlol/bot", "/public/imagenes-randlol/Position_Gold-Bot.png")
+	web.Static("/randlol/supp", "/public/imagenes-randlol/Position_Gold-Support.png")
 	web.Listen(":403")
 }
