8aa3aaae5718b487f2bbefce741f112902131c97
cnlund <91978441+cnlund@users.noreply.github.com>
2023-12-15 14:25:04 -0500

    avance 6

--- private/randlol.go
+++ private/randlol.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/Kyagara/equinox"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -10,6 +14,16 @@ func inicioHandler(c *fiber.Ctx) error {
 
 func randlolHandler(c *fiber.Ctx) error {
 	return c.SendFile("public/randlol.html")
+}
+
+func pruebaapiHandler(c *fiber.Ctx) error {
+	client, err := equinox.NewClient("RGAPI-5a4bccc9-8e19-4592-9d6a-989b18103e8d")
+	if err != nil {
+		fmt.Println("Error al crear el cliente de riot: ", err)
+	}
+	ctx := context.Background()
+	prueba, _ := client.DDragon.Champion.ByName(ctx, "13.24.1", "es_MX", "Aatrox")
+	return c.JSON(prueba)
 }
 
 func main() {
@@ -24,6 +38,6 @@ func main() {
 	web.Static("/randlol/mid", "/public/imagenes-randlol/Position_Gold-Mid.png")
 	web.Static("/randlol/bot", "/public/imagenes-randlol/Position_Gold-Bot.png")
 	web.Static("/randlol/supp", "/public/imagenes-randlol/Position_Gold-Support.png")
-	web.Static("/listachamps", "/champsdata.json")
+	web.Get("/pruebaapi", pruebaapiHandler)
 	web.Listen(":403")
 }
